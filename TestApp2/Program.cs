a872e0727d9f43a6837a7b30f9a49276423cf91f
Anton Darovsky <antondarovski97@gmail.com>
2022-06-19 18:17:53 +0300

    Swap min and max

---
+++ TestApp2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace TestApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = { 5, 2, 1, 4, 5, 8 };
+
+            int max = array1[0];
+            int min = array1[0];
+            for(int i = 1; i<array1.Length;i++)
+            {
+                if (array1[i] < min)
+                {
+                    min = array1[i];
+                }
+               else if (array1[i] > max)
+                {
+                    max = array1[i];
+                }
+
+            }
+            int maxIndex = Array.IndexOf(array1, max);
+            int minIndex = Array.IndexOf(array1, min);
+
+            array1[minIndex] = max;
+            array1[maxIndex] = min;
+
+            Console.WriteLine("Result = {0}", string.Join(", ", array1));
+
+        }
+    }
+}
